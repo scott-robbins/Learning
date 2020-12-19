c2f19988a972ac6cc2aa80103136cc3d700dc130
TylersDurden <0ptik41@protonmail.com>
2020-12-18 22:51:02 -0500

    more simple C examples

--- Networking/simple_client.c
+++ Networking/simple_client.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[])
   socklen_t addr_size;
 
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
   /*---- Configure settings of the server address struct ----*/
   /* Address family = Internet */
   serverAddr.sin_family = AF_INET;
@@ -27,14 +26,11 @@ int main(int argc, char *argv[])
   serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
   /* Set all bits of the padding field to 0 */
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
   /*---- Connect the socket to the server using the address struct ----*/
   addr_size = sizeof serverAddr;
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-
   /*---- Read the message from the server into the buffer ----*/
   recv(clientSocket, buffer, 1024, 0);
-
   /*---- Print the received message ----*/
   printf("Data received: %s",buffer);   
 
