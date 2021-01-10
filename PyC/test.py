1d918596db95f5efb5365c612a791c223b3ec8b2
scott-robbins <tyl3r5durd3n@protonmail.com>
2021-01-10 18:32:18 -0500

    adding bitwise OR

--- PyC/test.py
+++ PyC/test.py
@@ -2,17 +2,19 @@ from ctypes import *
 import os
 
 if os.name == 'posix': # on linux
+    if not os.path.isfile('lib.so') and os.path.isfile('test.c'):
+        os.system('gcc -shared -fPIC -o lib.so test.c')
     lib = cdll.LoadLibrary("./lib.so")
 elif os.name == 'nt': # on windows
     lib = cdll.LoadLibrary("lib.dll")
 
 
-def show_bytes(buffer):
+def show_bytes(buffer, label):
     bytestr = ''
     data_size = lib.get_size(buffer_b)
     for i in range(data_size):
         bytestr += str(lib.bit_get(buffer, i))
-    print(bytestr)
+    print('%s\t%s' % (bytestr, label))
     return bytestr
 
 
@@ -34,24 +36,29 @@ p.num = 42
 lib.show_struct(byref(p))
 
 # Now try the bit operations
-buffer_a = chr(41)
+buffer_a = chr(128)
 buffer_b = chr(42)
-print('Buffer A and Buffer B:')
-show_bytes(buffer_a)
-show_bytes(buffer_b)
-# test trying to manually adjust bits
-# lib.bit_set(buffer_b, 0, 1)
-# lib.bit_set(buffer_b, 1, 1)
-# lib.bit_set(buffer_b, 3, 1)
-# lib.bit_set(buffer_b, 5, 1)
-# show_bytes(buffer_b)
+
+show_bytes(buffer_a, 'A')
+show_bytes(buffer_b, 'B')
+print('='*20)
 
 # what about XOR?
 buffer_c = chr(0)
-print('Attempting low level XOR:')
 lib.bit_xor(buffer_a, buffer_b, buffer_c, 8)
-show_bytes(buffer_c)
+show_bytes(buffer_c, 'A ^ B')
 
-print('Attempting to bit-shift result to left by 3')
+
+# trying AND
+buffer_c = chr(0)
+lib.bit_and(buffer_a, buffer_b, buffer_c, 8)
+show_bytes(buffer_c, 'A & B')
+
+# testing OR
+buffer_c = chr(0)
+lib.bit_or(buffer_a, buffer_b, buffer_c, 8)
+show_bytes(buffer_c, 'A || B')
+
+# Test Bit-Shift Left
 lib.bit_rot_left(buffer_c, 8, 3)
-show_bytes(buffer_c)
+show_bytes(buffer_c, '(A ^ B) << 3')
