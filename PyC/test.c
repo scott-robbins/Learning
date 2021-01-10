2dffb5655fc6882936e8c51ad5b316504abf26ee
scott-robbins <tyl3r5durd3n@protonmail.com>
2021-01-10 16:05:17 -0500

    adding bit operations and python extensions for these C functions

--- PyC/test.c
+++ PyC/test.c
@@ -3,8 +3,12 @@ Compile with:
 $ gcc -shared -fPIC -o lib.so test.c
 
 */
+#include <assert.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <wchar.h>
+#include <string.h>
+// #include "bit.h"
 
 void test_print(void){
 	printf("C inside Python :)\n");
@@ -33,4 +37,122 @@ void show_struct(const struct PyStruct* s){
 	printf("%s %d\n", s->str, s->num);
 }
 
+int get_size(const unsigned char *data){
+    return sizeof(data);
+}
 
+
+int bit_get(const unsigned char *bitsIn, int pos){
+    // allocate memory and copy over what was given
+    int len = sizeof(bitsIn);
+    assert(len >= 0);
+    unsigned char *bits = malloc(len);
+    if (bits == NULL) { abort(); }
+    memcpy(&bits, &bitsIn, len);
+
+    // now perform operation on that new memory
+    unsigned char   mask;
+    int             i;
+
+    // Set mask for the bit to get
+    mask = 0x80;
+    for (i = 0; i < (pos % 8); i++)
+        mask = mask >> 1;
+
+    // Get the Bit
+    int result = (((mask & bits[(int)(pos / 8)]) == mask) ? 1 : 0);
+    return result;
+}
+
+void bit_set(unsigned char *bitsIn, int pos, int state) {
+    // allocate memory and copy over what was given
+    int len = sizeof(bitsIn);
+    assert(len >= 0);
+    unsigned char *bits = malloc(len);
+    if (bits == NULL) { abort(); }
+    memcpy(&bits, &bitsIn, len);
+
+	unsigned char	mask;
+	int				i;
+	// set a mask for bit to get
+	mask = 0x80;
+	for (i = 0; i < (pos % 8); i++)
+		mask = mask >> 1;
+	// set the bit
+	if (state)
+		bits[pos / 8] = bits[pos / 8] | mask;
+	else
+		bits[pos / 8] = bits[pos / 8] & (~mask);
+
+	return;
+
+}
+
+void bit_xor(const unsigned char *bitsIn1,
+			 const unsigned char *bitsIn2,
+			 unsigned char *bitsxIn, int size){
+    // allocate memory and copy over what was given
+    int len1 = sizeof(bitsIn1);
+    assert(len1 >= 0);
+    unsigned char *bits1 = malloc(len1);
+    if (bits1 == NULL) { abort(); }
+    memcpy(&bits1, &bitsIn1, len1);
+
+    int len2 = sizeof(bitsIn1);
+    assert(len2 >= 0);
+    unsigned char *bits2 = malloc(len2);
+    if (bits2 == NULL) { abort(); }
+    memcpy(&bits2, &bitsIn2, len2);
+
+    int len3 = sizeof(bitsxIn);
+    assert(len3 >= 0);
+    unsigned char *bitsx = malloc(len3);
+    if (bitsx == NULL) { abort(); }
+    memcpy(&bitsx, &bitsxIn, len3);
+
+	int		i;
+	// compute the bitwise XOR (exclusive OR) of the the buffers given
+	for (i = 0; i < size; i++){
+		//compare each bit of each buffer, applying XOR
+		if (bit_get(bits1, i) != bit_get(bits2, i))
+			bit_set(bitsx, i, 1);
+		else // non matching bits are flipped
+			bit_set(bitsx, i, 0);
+	}
+
+	return;
+}
+
+void bit_rot_left(unsigned char *bitsIn, int size, int count) {
+    // allocate memory and copy over what was given
+    int len = sizeof(bitsIn);
+    assert(len >= 0);
+    unsigned char *bits = malloc(len);
+    if (bits == NULL) { abort(); }
+    memcpy(&bits, &bitsIn, len);
+	// Roate input buffer to the left by specified number of bits
+	int fbit, lbit, i, j;
+
+	if (size > 0) {
+		for (j = 0; j < count; j++){
+			for (i = 0; i <= ((size - 1) / 8); i++){
+				//get the bit to be shifted off current byte
+				lbit = bit_get(&bits[i], 0);
+
+				if (i == 0){
+					//save the bit shifted off the first byte for later
+					fbit = lbit;
+				} else {
+					// set the rightmost bit of the previous byte to the
+					// leftmost bit about to be shifted off the current byte
+					bit_set(&bits[i - 1], 7, lbit);
+				}
+				// shift current byte to the left
+				bits[i] = bits[i] << 1;
+			}
+			// set the rightmost bit of the buffer ti the but shifted off first byte
+			bit_set(bits, size - 1, fbit);
+		}
+	}
+	return;
+}
--- PyC/test.py
+++ PyC/test.py
@@ -1,11 +1,20 @@
 from ctypes import *
 import os
 
-
-if os.name == 'posix': # on linux 
-	lib = cdll.LoadLibrary("./lib.so")
+if os.name == 'posix': # on linux
+    lib = cdll.LoadLibrary("./lib.so")
 elif os.name == 'nt': # on windows
-	lib = cdll.LoadLibrary("lib.dll")
+    lib = cdll.LoadLibrary("lib.dll")
+
+
+def show_bytes(buffer):
+    bytestr = ''
+    data_size = lib.get_size(buffer_b)
+    for i in range(data_size):
+        bytestr += str(lib.bit_get(buffer, i))
+    print(bytestr)
+    return bytestr
+
 
 lib.test_print()
 lib.cprint("Hey C, It's Python!")
@@ -13,10 +22,9 @@ print(lib.add(1, 2))
 
 
 # Calling function with a structure
-# fields are defined by tuple with (fieldname, c_type) 
+# fields are defined by tuple with (fieldname, c_type)
 class PyStruct(Structure):
-	_fields_ = [("str",c_char_p),
-				  ("num",c_int)]
+    _fields_ = [("str",c_char_p),  ("num",c_int)]
 
 
 p = PyStruct()
@@ -24,3 +32,26 @@ p.str = "This is a test"
 p.num = 42
 # This isnt printing right.
 lib.show_struct(byref(p))
+
+# Now try the bit operations
+buffer_a = chr(41)
+buffer_b = chr(42)
+print('Buffer A and Buffer B:')
+show_bytes(buffer_a)
+show_bytes(buffer_b)
+# test trying to manually adjust bits
+# lib.bit_set(buffer_b, 0, 1)
+# lib.bit_set(buffer_b, 1, 1)
+# lib.bit_set(buffer_b, 3, 1)
+# lib.bit_set(buffer_b, 5, 1)
+# show_bytes(buffer_b)
+
+# what about XOR?
+buffer_c = chr(0)
+print('Attempting low level XOR:')
+lib.bit_xor(buffer_a, buffer_b, buffer_c, 8)
+show_bytes(buffer_c)
+
+print('Attempting to bit-shift result to left by 3')
+lib.bit_rot_left(buffer_c, 8, 3)
+show_bytes(buffer_c)
