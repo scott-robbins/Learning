bb69eb33b2324926e4b4b8e85222fa9da02b9f7f
TylersDurden <0ptik41@protonmail.com>
2021-01-24 00:48:04 -0500

    wrote my own program to overwrite variable using virtual memory based on reading through https://blog.holbertonschool.com/hack-the-virtual-memory-c-strings-proc/

--- Hybrid/loop.c
+++ Hybrid/loop.c
@@ -1,12 +1,16 @@
-/* gcc -Wall -Wextra -pedantic -Werror loop.c -o loop */
+/* 
+Taken From:
+https://blog.holbertonschool.com/hack-the-virtual-memory-c-strings-proc/
+compile with:
+gcc -Wall -Wextra -pedantic -Werror loop.c -o loop 
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 /**              
- * main - uses strdup to create a new string, loops forever-ever
- *                
+ * main - uses strdup to create a new string, loops forever-ever              
  * Return: EXIT_FAILURE if malloc failed. Other never returns
  */
 int main(void)
