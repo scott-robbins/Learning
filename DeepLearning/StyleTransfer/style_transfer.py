c22589a5077053fc65a323e1e85ca88f2a4e669f
scott-robbins <tyl3r5durd3n@protonmail.com>
2021-11-13 02:10:48 -0500

    fixing some typos

--- DeepLearning/StyleTransfer/style_transfer.py
+++ DeepLearning/StyleTransfer/style_transfer.py
@@ -158,9 +158,9 @@ def stylize(args):
 
 
 def main():
-main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
+	main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
 	subparsers = main_arg_parser.add_subparsers(title="subcommands", dest="subcommand")
-	# Check for args regarding training a model
+
 	train_arg_parser = subparsers.add_parser("train", help="parser for training arguments")
 	train_arg_parser.add_argument("--epochs", type=int, default=2,
 								  help="number of training epochs, default is 2")
@@ -189,17 +189,24 @@ main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-st
 								  help="weight for style-loss, default is 1e10")
 	train_arg_parser.add_argument("--lr", type=float, default=1e-3,
 								  help="learning rate, default is 1e-3")
-	# Check for Args regarding Evaluating existing Model 
+	train_arg_parser.add_argument("--log-interval", type=int, default=1000,
+								  help="number of images after which the training loss is logged, default is 500")
+	train_arg_parser.add_argument("--checkpoint-interval", type=int, default=2000,
+								  help="number of batches after which a checkpoint of the trained model will be created")
+
 	eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
 	eval_arg_parser.add_argument("--content-image", type=str, required=True,
 								 help="path to content image you want to stylize")
+	eval_arg_parser.add_argument("--content-scale", type=float, default=None,
+								 help="factor for scaling down the content image")
 	eval_arg_parser.add_argument("--output-image", type=str, required=True,
 								 help="path for saving the output image")
 	eval_arg_parser.add_argument("--model", type=str, required=True,
-								 help="saved model to be used for stylizing the image.")
+								 help="saved model to be used for stylizing the image. If file ends in .pth - PyTorch path is used, if in .onnx - Caffe2 path")
 	eval_arg_parser.add_argument("--cuda", type=int, required=True,
 								 help="set it to 1 for running on GPU, 0 for CPU")
-	
+	eval_arg_parser.add_argument("--export_onnx", type=str,
+								 help="export ONNX model to a given file")
 	args = main_arg_parser.parse_args()
 
 	if args.subcommand is None:
@@ -219,3 +226,4 @@ main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-st
 
 if __name__ == '__main__':
 	main()
+
