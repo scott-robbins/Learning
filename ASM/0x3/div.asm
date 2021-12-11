cbffaeb336764d3e402c13da12f941fb14b3fd7f
scott-robbins <tyl3r5durd3n@protonmail.com>
2021-12-11 09:10:02 -0500

    learning how to call printf from C and showing result of something simple

--- ASM/0x3/div.asm
+++ ASM/0x3/div.asm
@@ -4,6 +4,11 @@ global main
 extern printf     
 
 main:
+	mov edx, len
+	mov ecx, msg
+	mov eax, 4
+	int 0x80
+
 	xor eax, eax
 	xor ebx, ebx
 	xor ecx, ecx
@@ -18,11 +23,12 @@ main:
 	push format
 	call printf
 	add esp, 8
-	ret
+	ret 
 
 section	.data               
 	remainder db 0x0
-	format db "1337/42 = %d", 10, 0
-
+	format db "%d", 10, 0
+	msg db "1337/42 = ",0
+	len equ $ - msg
 
 ; nasm -f elf div.asm && gcc -m32 -o divide div.o
