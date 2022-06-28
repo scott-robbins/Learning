c5ebaec302c36678f0eb3e8d4c71a912a77f4982
cas1m1r <tyl3r5durd3n@gmail.com>
2022-06-28 16:33:07 -0400

    Create rot13.py

---
+++ Cryptography/Algorithms/rot13.py
@@ -0,0 +1,33 @@
+import string
+
+def encode(plaintext):
+	chars = list(string.printable)
+	lut = dict(zip(list(range(len(chars))), chars))
+	alphas = dict(zip(chars, list(range(len(chars)))))
+	result = ''
+	for letter in list(plaintext):
+		if letter in chars:
+			N = alphas[letter] + 13
+			print(f'{letter}:{alphas[letter]}')
+			if N >= len(chars):
+				N = N - len(chars)
+			result += lut[N]
+		else:
+			print(f'[!] unknown character {letter}')
+	return result
+
+def decode(ciphertext):
+	chars = list(string.printable)
+	lut = dict(zip(list(range(len(chars))), chars))
+	alphas = dict(zip(chars, list(range(len(chars)))))
+	result = ''
+	for letter in list(ciphertext):
+		if letter in chars:
+			N = alphas[letter] - 13
+			if N < 0:
+				N += len(chars)
+		result += lut[N]
+	return result
+
+if __name__ == '__main__':
+	print(decode('3PLorENEGvFGF5'))
