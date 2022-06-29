48859d382e721d6ac9e23669787c062e3f42f75a
cas1m1r <tyl3r5durd3n@gmail.com>
2022-06-28 21:33:29 -0400

    much cleanerimplementation

--- Cryptography/Algorithms/rot13.py
+++ Cryptography/Algorithms/rot13.py
@@ -1,30 +1,10 @@
 import string
 
 def encode(plaintext):
-	chars = list(string.printable)
-	lut = dict(zip(list(range(len(chars))), chars))
-	alphas = dict(zip(chars, list(range(len(chars)))))
-	result = ''
-	for letter in list(plaintext):
-		if letter in chars:
-			N = alphas[letter] + 13
-			print(f'{letter}:{alphas[letter]}')
-			if N >= len(chars):
-				N = N - len(chars)
-			result += lut[N]
-		else:
-			print(f'[!] unknown character {letter}')
-	return result
+	return ''.join([chr(ord(e)+13) for e in list(plaintext)])
 
 def decode(ciphertext):
-	chars = list(string.printable)
-	lut = dict(zip(list(range(len(chars))), chars))
-	alphas = dict(zip(chars, list(range(len(chars)))))
-	result = ''
-	for letter in list(ciphertext):
-		if letter in chars:
-			N = alphas[letter] - 13
-			if N < 0:
-				N += len(chars)
-		result += lut[N]
-	return result
+	return ''.join([chr(ord(e)-13) for e in list(ciphertext)])
+
+if __name__ == '__main__':
+	print(encode(decode('~{CyberArtists}~'))) 
