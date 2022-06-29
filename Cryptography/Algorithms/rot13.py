edfd9e76757b5742620ac5647759fa0d6f5e953d
cas1m1r <50683460+cas1m1r@users.noreply.github.com>
2022-06-28 21:39:50 -0400

    Update rot13.py

--- Cryptography/Algorithms/rot13.py
+++ Cryptography/Algorithms/rot13.py
@@ -5,6 +5,3 @@ def encode(plaintext):
 
 def decode(ciphertext):
 	return ''.join([chr(ord(e)-13) for e in list(ciphertext)])
-
-if __name__ == '__main__':
-	print(encode(decode('~{CyberArtists}~'))) 
